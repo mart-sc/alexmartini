df71ca50b41d6f7c9a38d5e68bfa4b6775a003ea
Alex Martini <alexmartini.sc@gmail.com>
2023-10-08 14:17:17 -0300

    Adição da google tag para o Google Analytics

--- layouts/default.php
+++ layouts/default.php
@@ -7,6 +7,16 @@
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
+    <!-- Google tag (gtag.js) -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-508G608MXY"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'G-508G608MXY');
+    </script>
+    
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
